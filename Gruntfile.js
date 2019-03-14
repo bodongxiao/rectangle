65c1e6edf05c85258d2cfc971b5b925035c1ea72
wangding <408542507@qq.com>
2019-03-14 19:48:57 +0800

    task 16

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,14 +23,55 @@ module.exports = function (grunt) {
       },
       src: '*.html'
                     
+    },
+    mocha: {
+      test: {
+        src: ['test/index.html'],
+      },
+      options: {
+        run: true,
+        reporter: 'Spec'
+      }
+    },
+    htmlmin: {
+      options: {
+        collapseWhitespace: true,
+        preserveLineBreaks: false
+                                      
+      },
+      files: {
+        src: './index.html',
+        dest: 'dist/index.html'
+                                      
+      }
+            
+    },
+    cssmin: {
+      'dist/rectangle.css': 'rectangle.css'
+                      
+    },
+    uglify: {
+      release:{
+        files: {
+          'dist/rectangle.js': 'rectangle.js',
+          'dist/calc.js': 'calc.js'
+                                              
+        }
+                      
+      }
+                    
     }
-      
   });
     
   grunt.loadNpmTasks('grunt-contrib-csslint');
   grunt.loadNpmTasks('grunt-htmlhint');
   grunt.loadNpmTasks('grunt-eslint');
+  grunt.loadNpmTasks('grunt-mocha');
+  grunt.loadNpmTasks('grunt-contrib-htmlmin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
                 
-  grunt.registerTask('default', ['htmlhint', 'csslint','eslint']);
+  grunt.registerTask('default', ['htmlhint', 'csslint','eslint','mocha']);
+  grunt.registerTask('minify'['htmlmin','cssmin','uglify']);
                     
 };
